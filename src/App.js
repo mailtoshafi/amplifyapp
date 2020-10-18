454800809b91053c71b216ddf97f16ee17029822
send2shafi <72552696+send2shafi@users.noreply.github.com>
2020-10-18 15:03:29 +0200

    V3

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-<h1>Hello from V2</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class Table extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			students:[
+				{id: 1, name: 'Mohamed', age: 35,email: 'a@b.com'},
+				{id: 1, name: 'Abdul', age: 35,email: 'a@b.com'},
+				{id: 1, name: 'Rahman', age: 35,email: 'a@b.com'},
+				{id: 1, name: 'Shafi', age: 35,email: 'a@b.com'}
+				]
+			}
+		}
 
-    </div>
-  );
+renderTableData() {
+	return this.state.students.map((student, index) => {
+		const {id,name,age,email } = student
+		return(
+			<tr key={id}>
+				<td>{id}</td>
+				<td>{name}</td>
+				<td>{age}</td>
+				<td>{email}</td>
+			</tr>
+			)
+		})
 }
 
-export default App;
+renderTableHeader() {
+      let header = Object.keys(this.state.students[0])
+      return header.map((key, index) => {
+         return <th key={index}>{key.toUpperCase()}</th>
+      })
+   }
+
+
+	render() {
+		return(
+			<div>
+				<h1 id='title'>React Dynamic Table</h1>
+				<table id='students'>
+					<tbody>
+						<tr>{this.renderTableHeader()}</tr>
+						{this.renderTableData()}
+					</tbody>
+				</table>
+			</div>
+			)
+		}
+	}
+
+export default Table;
